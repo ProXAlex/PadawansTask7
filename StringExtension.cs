953d8213ff7ffcba345f5f2efd77ea77770ea7a6
Alex <alex_goreloshev@mail.ru>
2019-02-17 12:12:36 +0300

    Implement OrderStringsByLength

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,8 +6,22 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if(array == null)
+                throw new ArgumentNullException("Agrument cannot be null");
+           Array.Sort(array, Comparison);
+        }
+
+        private static int Comparison(string x, string y)
+        {
+            if(x == null || y == null)
+                throw new ArgumentNullException("Agrument cannot be null");
+            if (x.Length > y.Length)
+                return 1;
+            if (x.Length == y.Length)
+                return 0;
+
+            return -1;
+
         }
     }
 }
